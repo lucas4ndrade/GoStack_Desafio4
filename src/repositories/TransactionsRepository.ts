9bcd79d0168c8efacce5929eb249629fb0784547
Lucas Andrade <lucas.andrade@peixeurbano.com>
2020-08-30 19:47:48 -0300

    GoStack - Desafio 4 concluído

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface transactionData {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,57 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    var totalIncome = 0;
+    var totalOutcome = 0;
+    var totalBalance = 0;
+
+    this.transactions.forEach(transaction => {
+      if(transaction.type == 'income') {
+        totalIncome += transaction.value;
+        totalBalance += transaction.value;
+      } else {
+        totalOutcome += transaction.value;
+        totalBalance -= transaction.value;
+      }
+    });
+
+    const balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: totalBalance
+    }
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title, value, type}: transactionData): Transaction {
+    const newTransaction = new Transaction({
+      title,
+      value,
+      type
+    })
+
+    this.transactions.push(newTransaction);
+    return newTransaction;
+  }
+
+  public validateType(type: string) {
+    if(type !== "income" && type !== "outcome") {
+      throw Error("Invalid attribute 'type', must be 'income' or 'outcome'")
+    }
+  }
+
+  public validateBalance(value:number, type:string) {
+    const invalidTransaction = type === "outcome" && 
+                              (this.getBalance().total - value) < 0
+
+    if(invalidTransaction) {
+      throw Error('Saldo inválido');
+    }
   }
 }
 
